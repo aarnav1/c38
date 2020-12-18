d33264a4b554d10888a76c9fa73d414fe023fae2
aarnav1 <65185590+aarnav1@users.noreply.github.com>
2020-12-18 15:33:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,37 +5,38 @@ var playerCount;
 var allPlayers;
 var distance = 0;
 var database;
-var CarRank;
 
 var form, player, game;
 
-var cars, car1, car2, car3, car4;
-var car1Img, car2Img, car3Img, car4Img;
+var runners, runner1, runner2, runner3, runner4;
 
-function preload(){
-  car1Img = loadImage("images/car1.png");
-  car2Img = loadImage("images/car2.png");
-  car3Img = loadImage("images/car3.png");
-  car4Img = loadImage("images/car4.png");
-  ground = loadImage("images/ground.png");
-  track1 = loadImage("images/track.jpg");
+var track, runner1_img, runner2_img, runner3_img, runner4_img, hurdle, invisibleGround1, invisibleGround2;
+
+function preload() {
+    hurdle = loadImage("../images/hurdle.png");
+    track = loadImage("../images/pla.jpg");
+    runner1_img = loadImage("b.png", "p.png", "y.png");
+    runner2_img = loadImage("b.png", "p.png", "y.png");
 }
 
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
-  database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
+function setup() {
+    canvas = createCanvas(displayWidth, window.innerHeight);
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
+function draw() {
+    if (playerCount === 2) {
+        game.update(1);
+    }
+    if (gameState === 1) {
+        clear();
+        game.play();
+    }
+    if (gameState === 2) {
+        game.end();
+    }
 }
